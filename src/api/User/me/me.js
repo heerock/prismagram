6c22c94fcf92d446731a8bc2f14925fab80b087c
KIMINJOONG <ijboym216@gmail.com>
2019-08-10 01:08:16 +0900

    Computed Fields in Prisma

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -9,9 +9,16 @@ export default {
             const userProfile = await prisma.user({ id: user.id });
             const posts = await prisma.user({ id: user.id }).posts();
             return {
-                userProfile,
+                user: userProfile,
                 posts
             }
+        }
+    },
+    // 일반 resolver
+    // parent는 위에 있는 resolver 즉 user임 나를 call한 resolver
+    User: {
+        fullName: (parent, __, { request }) => {
+            return `${parent.firstName} ${parent.lastName}`
         }
     }
 }
